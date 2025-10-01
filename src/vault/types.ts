33d1431d4d8b3a3261227da6c884ccb9c45459ed
jake <jake@cloudcruise.com>
2025-09-30 19:54:43 -0700

    update vault methods (#5)
    * update vault types
    
    * 0.0.2-alpha.1
    
    * 0.0.2-alpha.2

--- src/vault/types.ts
+++ src/vault/types.ts
@@ -27,17 +27,24 @@ export interface VaultEntry {
   ip_address?: string;
   session_id?: string;
   allow_multiple_sessions?: boolean;
+  prevent_concurrency_during_login?: boolean | null;
+  max_concurrency?: number | null;
+  expiry_time_from_last_use?: string | null;
+  expiry_time_from_session_data_set?: string | null;
+  tfa_method?: 'AUTHENTICATOR' | 'EMAIL' | 'SMS' | null;
   cookies?: any;
   local_storage?: any;
   session_storage?: any;
-  persist_cookies?: boolean;
-  persist_local_storage?: boolean;
-  persist_session_storage?: boolean;
+  persist_cookies?: boolean | null;
+  persist_local_storage?: boolean | null;
+  persist_session_storage?: boolean | null;
   cookie_domain_to_store?: string | null;
   proxy?: ProxyConfig;
   proxy_string?: string | null;
   headers?: VaultPostPutHeadersInBody[];
   created_at?: string | null;
+  session_data_set_at?: string | null;
+  effective_expires_at?: string | null;
 }
 
 
